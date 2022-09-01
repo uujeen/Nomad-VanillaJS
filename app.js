fd6d25d692e9d373a0d6ea85441fdc1b7aee0421
uujeen <rkddmlwls2@gmail.com>
2022-09-01 13:52:30 +0900

    Using const, let, var

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 1; // const can't be changed, let can be change later also you can use var, but nowadays, we don't
+const b = 5;
+let myName = "Eugene";
+
+console.log(a);
+console.log(b);
+console.log("hello " + myName);
+
+myName = "Eugene Kang"
+
+console.log("My name is " + myName);
