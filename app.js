a5f79ba6baccf9f95bf5804b5d846785699b482f
uujeen <rkddmlwls2@gmail.com>
2022-09-20 19:38:08 +0900

    Object calculator

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-// function is like Method in JAVA, argument is simillar with parameter in JAVA
-// function function's name(argument1, 2, ……){
-//      ……        
-// }
-function sayHello(nameOfPerson, age) {
-    console.log("Hello my name is " + nameOfPerson + " And I'm " + age);
-}
-// *IMPORTANT* you should write "()" this, It's such a button to play to function
-sayHello("Eugene", 26);
-sayHello("Nico", 32);
-
-function plus(firstNumber, secondNumber){ // firstNumber, secondNumber is only exist "HERE", "function plus"
-    console.log(firstNumber + secondNumber);
-}
-// If you didn't to put in arguments in function to need arguments, It still can run, but there is a log such as "undefined, NaN(Not a Number"
-plus(5, 10);
-
-function devide(firstNumber, secondNumber){ // and also this firstNumber, secondNumber is only exist "HERE", but this is different with firstNumber, secondNumber in function plus
-    console.log(firstNumber / secondNumber);
-}
-devide(10,5);
-
-// our goal is make function like console.log() to player.sayHello()
-const player = {
-    name : "Eugene",
-    age : 26,
-    married : false,
-    sayHello : function(nameOfPerson){
-        console.log("Hello! " + nameOfPerson + " nice to meet you!");
+const calculator ={
+    plus: function(a,b){
+        return (a+b);
+    },
+    minus: function(a,b){
+        return (5-2);
+    },
+    divide: function(a,b){
+        console.log(a/b);
+    },
+    powerOf: function(a,b){
+        console.log(a**b);
     }
-}
+};
 
-player.sayHello("Eugene");
-player.sayHello("Nico");
+console.log(calculator.plus(5,2));
+calculator.minus(5,2);
+calculator.divide(4,2);
+calculator.powerOf(5,3);
