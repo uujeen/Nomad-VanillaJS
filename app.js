01726c6f8fe75c8b96b74256a496b2298ac583e2
uujeen <rkddmlwls2@gmail.com>
2022-09-23 17:10:15 +0900

    Conditional part

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-const calculator ={
-    plus: function(a,b){
-        return a + b;
-    },
-    minus: function(a,b){
-        return a - b;
-    },
-    times: function(a,b){
-        return a * b;
-    },
-    divide: function(a,b){
-        return a / b;
-    },
-    powerOf: function(a,b){
-        return a ** b;
-    }
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(2,3);
-const minusResult = calculator.minus(plusResult, 10);
-const timeResult = calculator.times(10,minusResult);
-const divResult = calculator.divide(timeResult, plusResult);
-const powResult = calculator.powerOf(divResult, minusResult);
-console.log(plusResult,minusResult,timeResult,divResult,powResult);
+
+console.log(age, typeof age);
+
+console.log(isNaN(age));
+if(isNaN(age) || age<0){
+    console.log("please write Number or positive Number");
+}
+else if(age < 18){
+    console.log("You are too young.");
+}
+else if(18<= age && age <= 50){
+    console.log("You can drink.");
+}
+else {
+    console.log("You are too old");
+}
